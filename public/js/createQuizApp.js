d8289d0c26407134be8ac11bdfd23ef9daf21e16
Samir Seetal <sseetal@gmail.com>
2015-01-29 15:59:49 +0000

    Delete quiz works

--- public/js/createQuizApp.js
+++ public/js/createQuizApp.js
@@ -576,10 +576,10 @@ angular.module('createQuizApp').controller('PreviewController', ['QuizData', '$l
 
 angular.module('createQuizApp').controller('DeleteController', ['QuizData', '$log', '$location', '$routeParams', function(QuizData, $log, $location, $routeParams){
     self.id = parseInt($routeParams.id);
-    if (self.id) {
-        QuizData.deleteQuiz(self.id, function() {                    
-            $log.debug("going to /");
-            $location.path("/");        
-        });                    
-    }
+    if(isNaN(self.id)) $location.path("/");
+    QuizData.deleteQuiz(self.id, function() {
+        $log.debug("going to /");
+        $location.path("/");
+    });
 }]);
+
