3aeb2dd1a2d88eccb4bb36e5cc424dbf673473e0
Samir Seetal <sseetal@gmail.com>
2015-04-27 23:09:28 +0100

    Make it work for test quilzail

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -11,6 +11,9 @@ function login(toggleLogin) {
   //var initParams = {"api": "98e820e8-77de-4964-8fa9-70434baf2e8b", "protocol": "http://","baseUrl": "localhost:8080/zzishapi/api/", "webUrl": "http://localhost:3000/","header": "X-ApplicationId","headerprefix": "","logEnabled": true};  
   var initParams = {"api": "2d14d1984a2e3293bd13aab34c85e2ea", "protocol": "http://","baseUrl": "test-api.zzish.com/api/", "webUrl": "http://test.zzish.com/"};
   // var initParams = "2d14d1984a2e3293bd13aab34c85e2ea";  
+  if (initParams.webUrl.indexOf("http://test")==0) {
+    url = "http://test.quizalize.com";
+  }
 
   var type = "redirect";
 
