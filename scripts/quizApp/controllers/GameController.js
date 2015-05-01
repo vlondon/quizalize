c74974b9b7cf2e4a7e70762c8f479a28260f7804
Samir Seetal <sseetal@gmail.com>
2015-05-01 10:56:59 +0100

    Add additional methods to GameController

--- scripts/quizApp/controllers/GameController.js
+++ scripts/quizApp/controllers/GameController.js
@@ -16,5 +16,19 @@ angular.module('quizApp').controller('GameController', ['QuizData', '$log', '$lo
 
     self.start = function(){
         $location.path("/quiz/" +  QuizData.chooseKind(0) + "/0");
-    };         
+    }; 
+
+    self.return = function() {
+        $location.path("/app/");
+    }
+
+    self.cancel = function() {
+        QuizData.confirmWithUser("Cancel Quiz","Are you sure you want to cancel '" + QuizData.currentQuizData.name+"'. You won't be able to continue this quiz.",function() {
+            $location.path("/app#/"); 
+            QuizData.cancelQuiz(QuizData.currentQuizData.uuid,function() {
+                
+            });
+        });
+    };        
+            
 }]);
