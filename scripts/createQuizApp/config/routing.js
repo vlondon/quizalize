791b36d120ba902339bb14cd1f046068fae8b310
Samir Seetal <sseetal@gmail.com>
2015-05-01 16:16:11 +0100

    Revert

--- scripts/createQuizApp/config/routing.js
+++ scripts/createQuizApp/config/routing.js
@@ -20,7 +20,7 @@ angular.module('createQuizApp')
             controller: "PreviewController",
             controllerAs: "preview"
         })
-        .when('/published/:id/:action', {
+        .when('/published/:id', {
             templateUrl: "/quiz/view/published",
             controller: "PublishedController",
             controllerAs: "publish"
