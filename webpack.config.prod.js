c47856a84a9701d8f0c1ef5f5f52d2bb59be05ea
Blai Pratdesabaa <hello@blaipratdesaba.com>
2015-05-20 22:35:13 +0100

    Missing parameter for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,7 +30,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                loader: 'style!css!sass'
+                loader: 'style!css!autoprefixer-loader?browsers=last 2 version!sass'
             }
         ]
     },
