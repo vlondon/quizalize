279d0643cc576e6714465b386989b08814d04407
Francesco Busiello <frabusiello@gmail.com>
2015-05-22 17:28:21 +0100

    Mobile homepage now responsive. style changes to quiz of the day.

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ Endpoints for students: actually we are making this client side
  */
 
 app.get('/', checkForMobile, quiz.landingpage5);
-app.get('/mobile', quiz.landingpage2);
+app.get('/mobile', quiz.landingpage5);
 app.get('/ie', quiz.landingpage3);
 app.get('/ks4-gcse-maths', quiz.landingpage4);
 app.get('/landing1', quiz.landingpage);
