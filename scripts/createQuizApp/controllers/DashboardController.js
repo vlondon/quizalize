27dde4b889a6d4301fc80bdc3b579b55a00b3a0d
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-05-26 22:01:46 +0100

    Removing react component

--- scripts/createQuizApp/controllers/DashboardController.js
+++ scripts/createQuizApp/controllers/DashboardController.js
@@ -1,30 +1,30 @@
-var React = require('react');
-var CQDashboard = require('createQuizApp/components/CQDashboard');
+// var React = require('react');
+// var CQDashboard = require('createQuizApp/components/CQDashboard');
 
 angular.module('createQuizApp')
   .controller('DashboardController', function(QuizData, $log, $http, $location, $scope){
 
         var self = this;
 
-        var renderReactComponent = function(){
-            React.render(
-                React.createElement(CQDashboard, {}),
-                document.getElementById('reactContainer')
-            );
-        };
-
-
-        var addReactComponent = function(){
-
-            setTimeout(renderReactComponent, 200);
-
-            $scope.$on('$destroy', function(){
-                React.unmountComponentAtNode(document.getElementById('reactContainer'));
-            });
-
-        };
-
-        addReactComponent();
+        // var renderReactComponent = function(){
+        //     React.render(
+        //         React.createElement(CQDashboard, {}),
+        //         document.getElementById('reactContainer')
+        //     );
+        // };
+        //
+        //
+        // var addReactComponent = function(){
+        //
+        //     setTimeout(renderReactComponent, 200);
+        //
+        //     $scope.$on('$destroy', function(){
+        //         React.unmountComponentAtNode(document.getElementById('reactContainer'));
+        //     });
+        //
+        // };
+        //
+        // addReactComponent();
 
         self.createQuiz = function() {
             if (!QuizData.getUser()) {
