ac4f90165425514ab15a32971cf894e050423da1
Samir Seetal <sseetal@gmail.com>
2015-06-09 14:20:05 +0100

    Redirect of Quizzes to put meta tag

--- routes/quiz.js
+++ routes/quiz.js
@@ -59,8 +59,25 @@ function getZzishParam(parse) {
 zzish.init(getZzishParam(true)); //TODO broken
 
 exports.index =  function(req, res) {
+    if (req.session.quiz===undefined) {
+        req.session.quiz = {};
+    }    
     res.render('index', {zzishapi : getZzishParam()});
 };
+
+exports.indexQuiz =  function(req, res) {
+    zzish.getPublicContent('quiz', req.params.id, function(err,result) {
+        if (!err) {
+            req.session.quiz = result; 
+        }
+        else {
+            req.session.quiz = {};
+        }
+        res.redirect('/app#/play/public/'+req.params.id);
+    })
+};
+
+
 
 exports.create =  function(req, res) {
     res.render('create',{zzishapi : getZzishParam()});
