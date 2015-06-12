356195e88837753906054d8025317847e27306d7
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-06-12 15:57:28 +0100

    Move quiz title to the top

--- scripts/createQuizApp/components/pages/CQCreate/CQCreate.jsx
+++ scripts/createQuizApp/components/pages/CQCreate/CQCreate.jsx
@@ -135,6 +135,19 @@ var CQCreate = React.createClass({
                                     </h2>
                                     <form role="form" className="form-horizontal">
                                         <div className="form-group">
+                                            <label className="col-sm-3 control-label">Quiz Title:<a data-toggle="popover" title="Quiz Title" data-content="Give your quiz a unique name so you can easily identify it." data-trigger="focus" data-placement="auto left" data-container="body" role="button" tabIndex="7" className="left-space glyphicon glyphicon-question-sign"></a></label>
+                                            <div className="col-sm-9">
+                                                <input id="question"
+                                                    type="text"
+                                                    value={this.state.quiz.name}
+                                                    onChange={this.handleChange.bind(this, 'name')}
+                                                    on-enter="ctrl.createQuiz();"
+                                                    ng-model="ctrl.quiz.name"
+                                                    placeholder="e.g. Plate Boundaries"
+                                                    autofocus="true"
+                                                    tabIndex="3"
+                                                    className="form-control"/><br/>
+                                            </div>
                                             <label className="control-label col-sm-3">
                                                 Subject:    <a data-toggle="popover" title="Quiz Subject" data-content="You can provide an optional subject to help organize your quizzes into different subject areas. This is optional." data-trigger="focus" data-placement="auto left" data-container="body" role="button" tabIndex="5" className="left-space glyphicon glyphicon-question-sign"></a>
                                         </label>
@@ -165,19 +178,7 @@ var CQCreate = React.createClass({
                                             className="form-control"/>
                                         <br/>
                                     </div>
-                                    <label className="col-sm-3 control-label">Quiz Title:<a data-toggle="popover" title="Quiz Title" data-content="Give your quiz a unique name so you can easily identify it." data-trigger="focus" data-placement="auto left" data-container="body" role="button" tabIndex="7" className="left-space glyphicon glyphicon-question-sign"></a></label>
-                                    <div className="col-sm-9">
-                                        <input id="question"
-                                            type="text"
-                                            value={this.state.quiz.name}
-                                            onChange={this.handleChange.bind(this, 'name')}
-                                            on-enter="ctrl.createQuiz();"
-                                            ng-model="ctrl.quiz.name"
-                                            placeholder="e.g. Plate Boundaries"
-                                            autofocus="true"
-                                            tabIndex="3"
-                                            className="form-control"/><br/>
-                                    </div>
+
                                     <div className="col-sm-4 col-sm-offset-4">
                                         <button type="button"
 
