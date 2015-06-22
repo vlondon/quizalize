e8ad3ba460a2db0796d793b13b6e8ebd75fb1e24
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-06-22 14:05:06 +0100

    Prevent IE caching .get calls

--- scripts/createQuizApp/actions/api/AppApi.js
+++ scripts/createQuizApp/actions/api/AppApi.js
@@ -1,4 +1,5 @@
 var request = require('superagent');
+var noCache = require('superagent-no-cache');
 var Promise = require('es6-promise').Promise;
 
 var AppApi = {
@@ -15,6 +16,7 @@ var AppApi = {
                 reject();
             } else {
                 request.get(`/create/${uuid}/apps/`)
+                .use(noCache)
                 .end(function(error, res){
                     if (error) {
                         reject();
@@ -38,6 +40,7 @@ var AppApi = {
                 reject();
             } else {
                 request.get(`/apps/${appId}`)
+                .use(noCache)
                 .end(function(error, res){
                     if (error) {
                         reject();
