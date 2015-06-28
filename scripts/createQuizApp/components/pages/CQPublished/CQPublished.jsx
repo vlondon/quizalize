e1d65e17acd5e268d76c72e4cf3e26cb573792a5
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-06-28 11:00:36 +0100

    Add and manage classes at the end of quiz creation

--- scripts/createQuizApp/components/pages/CQPublished/CQPublished.jsx
+++ scripts/createQuizApp/components/pages/CQPublished/CQPublished.jsx
@@ -182,29 +182,9 @@ var CQPublished = React.createClass({
                     </p>
                 </div>
 
-                <CQViewClassList/>
-                <div className="cq-published__assign">
-                    Set as a class game (or homework)…
-                    <ul className="list-unstyled">
-                        {this._showGroupsList().map( (classN) => {
-                            return (
-                                <li key={classN.value}>
-                                    <input type="checkbox" id={classN.value}/>
-                                    <label htmlFor={classN.value}>
-                                        &nbsp;{classN.label}
-                                    </label>
+                <CQViewClassList
+                    quizId={this.props.quizId}/>
 
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <input type="text" placeholder="Enter a new class name"/>
-                    <br/>
-                    <button className="btn btn-default"
-                        onClick={this.handleClick}>
-                        Done - Go to your dashboard
-                    </button>
-                </div>
 
 
                 <div className="pricing">
