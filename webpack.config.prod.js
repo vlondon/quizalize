9467761bae813f1b24efef9262c7615250b8fb1a
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-07-08 16:20:21 +0100

    Fix production build script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,6 +12,7 @@ module.exports = {
         quiz: 'quiz.js',
         quizApp: 'quizApp.js',
         cqApp: ['createQuizApp/styles/createQuizApp', 'createQuizApp/CQApp.js'],
+        pqApp: ['playQuizApp/styles/PQStyles', 'playQuizApp/index'],
         vendor: ['fastclick', 'react', 'superagent', 'object-assign']
     },
     plugins: [
