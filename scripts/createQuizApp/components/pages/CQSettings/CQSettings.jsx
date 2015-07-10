dd460e661a6e4ca89df066fafb250be1914ae95c
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-07-10 15:46:46 +0100

    Settings

--- scripts/createQuizApp/components/pages/CQSettings/CQSettings.jsx
+++ scripts/createQuizApp/components/pages/CQSettings/CQSettings.jsx
@@ -1,59 +1,95 @@
-
+/* @flow */
 var React = require('react');
-var CQPageTemplate = require('createQuizApp/components/CQPageTemplate');
-var CQViewProfilePicture = require('createQuizApp/components/views/CQViewProfilePicture');
-var UserStore = require('createQuizApp/stores/UserStore');
-var UserActions = require('createQuizApp/actions/UserActions');
+
+import type {User} from './../../../stores/UserStore';
+var CQPageTemplate = require('./../../../components/CQPageTemplate');
+var CQViewProfilePicture = require('./../../../components/views/CQViewProfilePicture');
+var UserStore = require('./../../../stores/UserStore');
+var UserActions = require('./../../../actions/UserActions');
 var assign = require('object-assign');
-var router = require('createQuizApp/config/router');
+var router = require('./../../../config/router');
 
-var facebookSDK = require('createQuizApp/config/facebookSDK');
+var facebookSDK = require('./../../../config/facebookSDK');
 
-var CQSettings = React.createClass({
+import {urlParams} from './../../../utils';
 
-    getInitialState: function() {
-        var user = UserStore.getUser();
-        return {
-            user
+type Params = {
+    redirect: string;
+}
+type State = {
+    user: User;
+    params: Params;
+}
+
+var required = ['name', 'school', 'location'];
+export default class CQSettings extends React.Component {
+
+    state: State;
+
+    constructor (props: any) {
+        super(props);
+        var user:User = UserStore.getUser();
+        var params = urlParams();
+        var canSave = false;
+        this.state =  {
+            user,
+            params,
+            canSave
         };
-    },
-    componentDidMount: function() {
-        facebookSDK.load();
-    },
 
-    handleSave: function(){
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    componentDidMount() {
+        facebookSDK.load();
+    }
+
+    handleSave(){
         UserActions.update(this.state.user)
-            .then( ()=> router.setRoute('/quiz/quizzes'));
-    },
-    handleChange: function(field, event){
+            .then( ()=> {
+                if (this.state.params.redirect){
+                    router.setRoute(this.state.params.redirect);
+                } else {
+                    router.setRoute('/quiz/quizzes');
+                }
+            });
+    }
+
+    handleChange(field: string, event: Object){
 
         var user = assign({}, this.state.user);
         user.attributes[field] = event.target.value;
         this.setState({user});
 
-    },
+    }
 
-    handleNameChange: function(event){
+    handleNameChange(event: Object){
 
         var user = assign({}, this.state.user);
+
         user.name = event.target.value;
         this.setState({user});
 
-    },
+    }
 
-    handleProfilePicture: function(){
+    handleProfilePicture(){
         facebookSDK.getProfilePicture()
             .then((profilePictureUrl) => {
-                console.log('we got', profilePictureUrl);
+
                 var user = assign({}, this.state.user);
+
                 user.avatar = profilePictureUrl;
+
                 this.setState({user}, ()=>{
                     UserActions.update(this.state.user);
                 });
-            });
-    },
 
-    render: function() {
+            });
+    }
+
+    render() {
+
         return (
             <CQPageTemplate className="cq-container cq-settings">
 
@@ -63,16 +99,16 @@ var CQSettings = React.createClass({
 
                 <div className="cq-settings__profile ">
                     <div className="cq-settings__profile-item form-group">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">Name <small>(required)</small></label>
                         <input type="text" id="name"
                             className="form-control"
-                            placeholder = "e.g. Mr Smith"
+                            placeholder = "e.g. John Smith"
                             onChange={this.handleNameChange}
                             value={this.state.user.name}/>
                     </div>
 
                     <div className="cq-settings__profile-item form-group">
-                        <label htmlFor="school">School name / company</label>
+                        <label htmlFor="school">School name / company <small>(required)</small></label>
                         <input type="text" id="school"
                             className="form-control"
                             placeholder = "e.g. City School Academy"
@@ -84,17 +120,37 @@ var CQSettings = React.createClass({
                        <label htmlFor="url">School Website</label>
                        <input type="url" id="url"
                            className="form-control"
+                           placeholder="e.g. http://www.school.com"
                            onChange={this.handleChange.bind(this, 'url')}
                            value={this.state.user.attributes.url}/>
                     </div>
 
                     <div className="cq-settings__profile-item form-group">
-                        <label htmlFor="location">Location</label>
+                        <label htmlFor="location">Location <small>(required)</small></label>
                         <input type="text" id="location"
                             className="form-control"
                             placeholder = "e.g. London"
                             onChange={this.handleChange.bind(this, 'location')}
                             value={this.state.user.attributes.location}/>
+                    </div>
+
+                    <div className="cq-settings__profile-item form-group">
+                        <label htmlFor="location">Age group taught</label>
+                        <input type="text" id="location"
+                            className="form-control"
+                            placeholder = "e.g. Year 3 / Grade 3"
+                            onChange={this.handleChange.bind(this, 'ageTaught')}
+                            value={this.state.user.attributes.ageTaught}/>
+                    </div>
+
+
+                    <div className="cq-settings__profile-item form-group">
+                        <label htmlFor="location">Subject taught</label>
+                        <input type="text" id="location"
+                            className="form-control"
+                            placeholder = "e.g. Biology and Chemistry"
+                            onChange={this.handleChange.bind(this, 'subjectTaught')}
+                            value={this.state.user.attributes.subjectTaught}/>
                     </div>
 
 
@@ -121,9 +177,8 @@ var CQSettings = React.createClass({
                 </div>
             </CQPageTemplate>
         );
+
     }
 
 
-});
-
-module.exports = CQSettings;
+}
