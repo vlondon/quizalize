8f4f675aa8bd2e3e144bc223cb5207cc72c0d609
Samir Seetal <sseetal@gmail.com>
2015-08-24 10:33:00 +0100

    Add check for zzish (only admin)

--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,18 @@ var APP_CONTENT_TYPE    = "app";
 var async = require('async');
 var QUIZ_CONTENT_TYPE   = "quiz";
 
-var Zzish = require('zzish');
-var zzish_db = new Zzish();
-var adminResult = {
-    apiUrl: config.apiUrlAdmin,
-    appToken: config.appTokenAdmin,
-    wso2: "true",
-    log: false
-};
-console.log("Init", adminResult);
-zzish_db.init(adminResult);
+if (process.env.admin=="true") {
+    var Zzish = require('zzish');
+    var zzish_db = new Zzish();
+    var adminResult = {
+        apiUrl: config.apiUrlAdmin,
+        appToken: config.appTokenAdmin,
+        wso2: "true",
+        log: false
+    };
+    console.log("Init", adminResult);
+    zzish_db.init(adminResult);
+}
 
 
 
