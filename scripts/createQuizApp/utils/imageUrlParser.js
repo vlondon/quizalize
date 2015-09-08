d8b6c3f5fc8a2d56b43e1b25548732883c46be19
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-09-08 15:40:25 +0100

    Handle agnostic urls

--- scripts/createQuizApp/utils/imageUrlParser.js
+++ scripts/createQuizApp/utils/imageUrlParser.js
@@ -1,7 +1,8 @@
 var imageUrlParser = function(url){
     var CDNURL = 'https://d15tuytjqnsden.cloudfront.net/';
 
-    if (url && url.indexOf('http') === -1) {
+    if ((url && url.indexOf('http') === -1) && (url && url.indexOf('//') !== 0)) {
+        console.log('adding url to', url, url && url.indexOf('//') !== 0);
         url = CDNURL + url;
     }
 
