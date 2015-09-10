c73ec6a301c632dd2424154d44750a8e3f839f8c
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-09-10 11:46:10 +0100

    WIP

--- routes/graphql/graphQLQuiz.js
+++ routes/graphql/graphQLQuiz.js
@@ -1,43 +1,53 @@
 var zzish = require("../../zzish");
 
-class Quiz extends Object {
+class Quiz  {
     constructor (obj){
-        super(obj);
         Object.assign(this, obj);
     }
+    toJSON(){
+        return this;
+    }
+
 }
 
-var performQuery = function(mongoQuery) {
 
-    return new Promise((resolve, reject)=>{
-        console.log('zzish hit');
-        zzish.searchPublicContent('quiz', mongoQuery, function(err, resp){
-            if (resp) {
-                resolve(resp);
-            } else {
-                reject(error);
-            }
-        });
-    });
-};
+class Quizzes {
 
+    constructor(){
+        this.quizzes = [];
+    }
 
+    getQuizById(quizId){
+        var quiz = this.quizzes.filter(q=> quizId === q.uuid)[0];
 
-var QuizActions = {
-    getQuizById: function(uuid) {
+        if (quiz === undefined){
+            console.log('form zzish');
+            return this.loadUser(userId);
+        } else {
+            console.log('form chache');
+            return user.toJSON();
+        }
+    }
+
+    loadQuiz(quizId){
         return new Promise(function(resolve, reject){
 
-            zzish.getPublicContent('quiz', uuid, function(err, resp){
+            zzish.getPublicContent('quiz', quizId, (err, resp) => {
                 if (err) {
                     reject(err);
                 } else {
                     var quiz = new Quiz(resp);
+                    console.log('userquizzes', quiz);
+                    this.quizzes.push(quiz);
                     resolve(quiz);
                 }
             });
+
         });
-    },
-    getUserQuizzes: function(profileId){
+
+    }
+
+    getUserQuizzes (profileId){
 
         let now = Date.now();
         let lastYear = now - 365 * 7 * 24 * 60 * 60 * 1000;
@@ -55,8 +65,63 @@ var QuizActions = {
         };
 
 
+
         return performQuery(mongoQuery);
     }
+
+}
+
+
+var performQuery = function(mongoQuery) {
+
+    return new Promise((resolve, reject)=>{
+        zzish.searchPublicContent('quiz', mongoQuery, function(err, resp){
+            if (resp) {
+                resolve(resp);
+            } else {
+                reject(error);
+            }
+        });
+    });
 };
 
-export default QuizActions;
+//
+//
+// var QuizActions = {
+//     getQuizById: function(uuid) {
+//         return new Promise(function(resolve, reject){
+//
+//             zzish.getPublicContent('quiz', uuid, function(err, resp){
+//                 if (err) {
+//                     reject(err);
+//                 } else {
+//                     var quiz = new Quiz(resp);
+//                     resolve(quiz);
+//                 }
+//             });
+//         });
+//     },
+//     getUserQuizzes: function(profileId){
+//
+//         let now = Date.now();
+//         let lastYear = now - 365 * 7 * 24 * 60 * 60 * 1000;
+//
+//         let mongoQuery = {
+//             updated: {
+//                 $gt: lastYear
+//             },
+//             published: "published",
+//             name: {
+//                 $regex: '', $options: 'i'
+//             },
+//             profileId
+//
+//         };
+//
+//
+//         return performQuery(mongoQuery);
+//     }
+// };
+
+var quizzes = new Quizzes();
+export default quizzes;
