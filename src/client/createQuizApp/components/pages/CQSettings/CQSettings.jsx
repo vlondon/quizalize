b5ea91a01160f784de3ff973ba6e6e905694931d
Francesco Busiello <frabusiello@gmail.com>
2015-09-29 15:36:32 +0100

    admin stats fix

--- src/client/createQuizApp/components/pages/CQSettings/CQSettings.jsx
+++ src/client/createQuizApp/components/pages/CQSettings/CQSettings.jsx
@@ -266,7 +266,7 @@ export default class CQSettings extends React.Component {
             );
         }
 
-        message = this.state.isNew ? 'We need some extra information' : 'Settings';
+        message = this.state.isNew ? 'Complete your registration' : 'Settings';
 
         var editProfile = () => {
             if (!this.state.isNew) {
