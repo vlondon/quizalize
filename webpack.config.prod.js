16fcb0c1484090c03ccb4e43dfc4e2b07a2d6d18
Francesco Busiello <frabusiello@gmail.com>
2015-10-12 16:57:12 +0100

    Cherry pick publishers page and admin stats changes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,6 +13,7 @@ module.exports = {
         quiz: 'quiz.js',
         quizApp: 'quizApp.js',
         home: 'home.js',
+        publishers: 'publishers.js',
         cqApp: ['createQuizApp/styles/createQuizApp', 'createQuizApp/CQApp.js'],
         vendor: ['fastclick', 'react', 'superagent']
     },
