902827b6802fe12ae54166bce0b9cbecba7c945c
Blai Pratdesaba <hello@blaipratdesaba.com>
2015-10-13 18:35:09 +0100

    Tweak live view on homepage

--- src/client/liveBeta/components/Tweenable.js
+++
@@ -1,8 +0,0 @@
-/* Decorator to add react-tween-state to class */
-import TweenState from 'react-tween-state';
-
-export default SubComponent => {
-    Object.assign(SubComponent.prototype, TweenState.Mixin);
-    console.log('SubComponent,', SubComponent);
-    return SubComponent;
-};
