81c12b1dc18e933273f43501178533e046a9c552
Francesco Busiello <frabusiello@gmail.com>
2015-11-09 20:24:24 +0000

    Added error handling to some functions in quiz.js and user.js

--- src/server/routes/quiz.js
+++ src/server/routes/quiz.js
@@ -350,17 +350,26 @@ exports.deleteTopic = function(req, res){
 };
 
 exports.getQuiz = function(req, res){
-    var id = req.params.id;
-    var profileId = req.session.user.uuid;
+    if (req && req.session && req.session.user && req.session.user.uuid){
+        var id = req.params.id;
+        var profileId = req.session.user.uuid;
 
-    // var profileId = req.params.profileId;
-    logger.debug('Loading Quiz', id);
-    zzish.getContent(profileId, QUIZ_CONTENT_TYPE, id, function(err, resp){
-        logger.debug('Quiz loaded', id, err, resp);
-        if (!handleError(err, res)) {
-            res.send(resp);
-        }
-    });
+        // var profileId = req.params.profileId;
+        logger.debug('Loading Quiz', id);
+        zzish.getContent(profileId, QUIZ_CONTENT_TYPE, id, function(err, resp){
+            logger.debug('Quiz loaded', id, err, resp);
+            if (!handleError(err, res)) {
+                res.send(resp);
+            }
+        });
+    }
+    else {
+        email.sendEmailTemplate("'Quizalize Team' <team@quizalize.com>", ['team@quizalize.com'], 'Failed to get quiz', 'error', {
+          error: "Failed to getQuiz, quiz.js line 352",
+          message: req,
+          parameters: ""
+        });
+    }
 };
 
 exports.deleteQuiz = function(req, res){
