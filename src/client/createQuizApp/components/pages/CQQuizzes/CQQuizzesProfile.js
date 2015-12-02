2393cb75badbc23ad484475ea0d834d340c0c90f
Amanda Chen <jryingchen@gmail.com>
2015-12-02 21:24:43 +0000

    Put quiz actions in a dropdown

--- src/client/createQuizApp/components/pages/CQQuizzes/CQQuizzesProfile.js
+++ src/client/createQuizApp/components/pages/CQQuizzes/CQQuizzesProfile.js
@@ -1,7 +1,7 @@
 /* @flow */
 import React, { PropTypes } from 'react';
 import { GroupStore } from './../../../stores';
-import { CQPublishQuiz } from './../../../components';
+import { CQPublishQuiz, CQDropdown } from './../../../components';
 
 import { QuizActions } from './../../../actions';
 import { router }  from './../../../config';
@@ -15,6 +15,8 @@ class CQQuizzesProfile extends React.Component {
     props: Props;
     constructor(props: Props){
         super(props);
+        this.handleMenuContainer = this.handleMenuContainer.bind(this);
+        this.handleMenuItem = this.handleMenuItem.bind(this);
         this.handleShare = this.handleShare.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
         this.handlePreview = this.handlePreview.bind(this);
@@ -22,8 +24,16 @@ class CQQuizzesProfile extends React.Component {
         this.handleAssignHomework = this.handleAssignHomework.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
     }
-    handleShare(ev : Event){
+
+    handleMenuContainer(ev : Event){
         ev.stopPropagation();
+    }
+
+    handleMenuItem(item : Object){
+        item.handler();
+    }
+
+    handleShare(){
         router.setRoute(`/quiz/published/${this.props.quiz.uuid}/share`);
     }
     handleEdit(ev : Event){
@@ -33,8 +43,7 @@ class CQQuizzesProfile extends React.Component {
         }
 
     }
-    handlePreview(ev : Event){
-        ev.stopPropagation();
+    handlePreview(){
         var quiz = this.props.quiz;
         sessionStorage.setItem('mode', 'teacher');
         QuizActions.loadQuiz(quiz.uuid).then( (quiz) => {
@@ -56,16 +65,14 @@ class CQQuizzesProfile extends React.Component {
         }
     }
 
-    handleAssignHomework(ev : Event){
-        ev.stopPropagation();
+    handleAssignHomework(){
         var quiz = this.props.quiz;
         if (quiz){
             router.setRoute(`/quiz/published/${quiz.uuid}/assign?homework=true`);
         }
     }
 
-    handleDelete(ev : Event){
-        ev.stopPropagation();
+    handleDelete(){
         var quiz = this.props.quiz;
         var found = false;
         var groupContents = GroupStore.getGroupsContent();
@@ -96,38 +103,78 @@ class CQQuizzesProfile extends React.Component {
         }
     }
     render () : any {
+        var moreAction = [
+            {
+                value: "more",
+                name: "More Actions",
+                handler: this.handleMenuItem,
+                disabled: true
+            },
+            {
+                value: "share",
+                name: "Share",
+                handler: this.handleShare
+            },
+            {
+                value: "preview",
+                name: "Preview",
+                handler: this.handlePreview
+            },
+            {
+                value: "homework",
+                name: "Set as homework",
+                handler: this.handleAssignHomework
+            },
+            {
+                value: "delete",
+                name: "Delete",
+                handler: this.handleDelete
+            },
+        ];
+
+        var actionDropDown =  (
+            <span onClick={this.handleMenuContainer} className="cq-quizzes__dropdown--moreaction">
+                <CQDropdown
+                    values={moreAction}
+                    onChange={this.handleMenuItem}
+                    selected="more"/>
+            </span>
+        );
+
         return (
             <div>
 
                 <CQPublishQuiz className="cq-quizzes__button--publish" quiz={this.props.quiz}/>
 
-                <button className="cq-quizzes__button--share" onClick={this.handleShare}>
-                    <span className="fa fa-share"></span> Share
-                </button>
-
                 <button className="cq-quizzes__button--edit" onClick={this.handleEdit}>
                     <span className="fa fa-pencil"></span> Edit
-                </button>
-
-                <button className="cq-quizzes__button--preview" onClick={this.handlePreview}>
-                    <span className="fa fa-search"></span> Preview
                 </button>
 
                 <button className="cq-quizzes__button--assign" onClick={this.handleAssign}>
                     <span className="fa fa-users"></span> Play in class
                 </button>
 
-                <button className="cq-quizzes__button--assign" onClick={this.handleAssignHomework}>
-                    <span className="fa fa-users"></span> Set as homework
-                </button>
-
-                <button className="cq-quizzes__button--delete" onClick={this.handleDelete}>
-                    <span className="fa fa-trash-o"></span>
-                </button>
+                {actionDropDown}
 
             </div>
 
         );
+        //
+        // <button className="cq-quizzes__button--share" onClick={this.handleShare}>
+        //     <span className="fa fa-share"></span> Share
+        // </button>
+        //
+        // <button className="cq-quizzes__button--preview" onClick={this.handlePreview}>
+        //     <span className="fa fa-search"></span> Preview
+        // </button>
+        //
+        // <button className="cq-quizzes__button--assign" onClick={this.handleAssignHomework}>
+        //     <span className="fa fa-users"></span> Set as homework
+        // </button>
+        //
+        // <button className="cq-quizzes__button--delete" onClick={this.handleDelete}>
+        //     <span className="fa fa-trash-o"></span>
+        // </button>
     }
 }
 CQQuizzesProfile.propTypes = {
